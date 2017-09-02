b21911ea40ffbc4d1f841d8ca154868be085219b
21k <root@tmxk.org>
2017-09-02 13:57:18 +0800

    Update filter.ts

--- filter.ts
+++ filter.ts
@@ -1,43 +1,98 @@
-export function en(str) {
-    str = (str || "").toString();
-    const character = {
+var tool={};
+module.exports=tool;
+       
+/**
+ *
+ * @param str - 编码html
+ */
+tool.en = function(str) {
+    str = (str || '').toString();
+    var character = {
         '<': '&lt;',
         '>': '&gt;',
         '&': '&amp;',
         '"': '&quot;',
-        "'": '&#039;'
+        '\'': '&#039;'
     };
-    return function () {
-        return str.replace(/[<>&"']/g,
-            function (c) {
-                return character[c];
-            }
-        );
+    return function() {
+        return str.replace(/[<>&"']/g, function(c) {
+            return character[c];
+        });
     }();
-}
+};
 
+/**
+ * @param htmlStr string
+ * @param whiteTags optional String 'a div p span'
+ * @param spliter optional String ' '
+ */
+tool.secHtml = function(htmlStr, whiteTags, spliter) {
+    var rootTagName = Math.random().toString(36).replace('0.', 'byted');
+    var dom = (new DOMParser)
+        .parseFromString('<' + rootTagName + '>' + (htmlStr || '') + '</' + rootTagName + '>', 'text/html')
+        .getElementsByTagName(rootTagName)[0];
+    var attrFilterRules = [
+        {
+            name: 'rull-style',
+            attrs: ['style'],
+            reg: /expression\(.*\)/i,
+            replaceValue: ''
 
-export function filterHtml(s, whiteTags = ["var","p", "div", "span", "img", "a", "strong", "em", "ol", "li", "i", "pre", "br", "sub", "sup", "ul", "section", "h1", "h2", "h3", "h4", "h5", "h6", "b", "u", "s", "del", "strong", "big", "small", "strike", "kbd", "code", "address", "cite", "dl", "dd", "dt", "caption", "font", "hr", "nobr", "center", "table","thead", "tfoot","tr", "td", "tbody", "th", "blockquote"]) {
-    let $$ = window["jQuery"];
-    let vDom = $$("<section>" + s + "</section>");
-    let onEvent = /on(a(bort|fterprint)|b(eforeprint|eforeunload|lur)|c(anplay|anplaythrough|hange|lick|ontextmenu)|d(blclick|rag|ragend|ragenter|ragleave|ragover|ragstart|rop|urationchange)|e(mptied|nded|rror|rror)|f(ocus|ormchange|orminput)|h(aschange)|i(nput|nvalid)|k(eydown|eypress|eyup)|l(oad|oadeddata|oadedmetadata|oadstart)|m(essage|ousedown|ousemove|ouseout|ouseover|ouseup|ousewheel)|o(ffline|nline)|p(agehide|ageshow|ause|lay|laying|opstate|rogress)|r(atechange|eadystatechange|edo|eset|esize)|s(croll|eeked|eeking|elect|talled|torage|ubmit|uspend)|t(imeupdate)|u(ndo|nload)|v(olumechange)|w(aiting))/g;
-    whiteTags = whiteTags.map(tag=>tag.toLowerCase());
-    vDom.find("*").each((index, node)=> {
-        if (whiteTags.indexOf(node.tagName.toLowerCase()) > -1) {
-            $$(node.attributes).each((index, attr)=> {
-                if (["src", "href"].indexOf(attr.nodeName) > -1) {
-                    if (!/^(https?:\/\/|\/\/)/.test(attr.nodeValue)) {
-                        attr.nodeValue = ""
+        },
+        {
+            name: 'rull-href',
+            attrs: ['src', 'href'],
+            reg: /^(javascript|vbscript|jscript|ecmascript):|data:text\/html/i,
+            replaceValue: ''
+
+        },
+        {
+            name: 'rule-onevent',
+            attrs: (function() {
+                var tags = [];
+                for (var attr in dom) {
+                    if (attr.indexOf('on') === 0) {
+                        tags.push(attr);
                     }
                 }
-                if (onEvent.test(attr.nodeName)) {
-                    attr.nodeValue = ""
-                }
-            })
+                return tags;
+            })(),
+            replaceValue: ''
+
         }
-        else {
-            node.outerHTML = en(node.outerHTML)
+    ];
+    if (spliter === void 0) {
+        spliter = /\s+/g;
+    }
+    if (whiteTags === void 0) {
+        whiteTags = ['A', 'ADDRESS', 'B', 'BIG', 'BLOCKQUOTE', 'BR', 'CAPTION', 'CENTER', 'CITE', 'CODE', 'DD',
+            'DEL', 'DIV', 'DL', 'DT', 'EM', 'FONT', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'HR', 'I', 'IMG', 'KBD',
+            'LI', 'NOBR', 'OL', 'P', 'PRE', 'S', 'SECTION', 'SMALL', 'SPAN', 'STRIKE', 'STRONG', 'STRONG', 'SUB',
+            'SUP', 'SVG', 'TABLE', 'TBODY', 'TD', 'TFOOT', 'TH', 'THEAD', 'TR', 'U', 'UL', 'VAR'];
+    } else {
+        whiteTags = whiteTags.split(spliter).map(function(tag) {
+            return tag.toUpperCase();
+        });
+    }
+    var toArray = function(obj) {
+        return [].slice.apply(obj);
+    };
+    toArray(dom.querySelectorAll('*')).forEach(function(node, i) {
+        if (whiteTags.indexOf(node.tagName) > -1) {
+            toArray(node.attributes).forEach(function(attr) {
+                attrFilterRules.some(function(rule) {
+                    var tagMatched = rule.attrs.indexOf(attr.nodeName) > -1;
+                    if (tagMatched) {
+                        if ((rule.reg && rule.reg.test(attr.nodeValue.replace(/\s/g, ''))) || !rule.reg) {
+                            attr.nodeValue = rule.replaceValue;
+                        }
+                    }
+                    return tagMatched;
+                });
+            });
+        } else {
+            node.outerHTML = tool.en(node.outerHTML);
         }
     });
-    return vDom.html()
-}
+    return dom.innerHTML;
+};
